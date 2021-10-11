901f46426e55e2e85fb2d7d50fb638ca672e58f3
JGouyau <Gouyau.jimmy@gmail.com>
2021-10-11 09:39:59 +0200

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Delegate
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            ShowMoney test = ShowMoneyEuro;
+            Console.WriteLine(test(25));
+            test = ShowMoneyDollar;
+            Console.WriteLine("\n"+test(25));
+
+            Console.Read();
+        }
+
+
+        public delegate string ShowMoney(int money);
+
+        public static string ShowMoneyEuro(int money)
+        {
+            return money + " €";
+        }
+
+        public static string ShowMoneyDollar(int money)
+        {
+            return "$"+0.86*money;
+        }
+    }
+}
